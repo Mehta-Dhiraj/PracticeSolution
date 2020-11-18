cd0efa90fa4acc03f9a80e9e5c2c6fbaf86971ca
Dhiraj <DK266107@gmail.com>
2020-11-18 11:03:43 +0530

    Fiboncii class added

---
+++ Practice/Fiboncci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Fiboncci
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                int a = 0, b = 1, c = 1;
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write(c + "\t");
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
